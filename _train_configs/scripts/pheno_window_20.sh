45136f678b79f21e1ca5d154b45b06e1dbe6b69e
Will Solow <soloww@oregonstate.edu>
2025-11-10 14:05:02 -0800

    window scripts

---
+++ _train_configs/scripts/pheno_window_20.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J pheno_param_mtl_window_20
+#SBATCH -o output/pheno_param_mtl_window_20.out
+#SBATCH -e output/pheno_param_mtl_window_20.err
+#SBATCH -p eecs,share,gpu
+#SBATCH -t 2-12:00:00
+#SBATCH --gres=gpu:1
+
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_20 --seed 0
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_20 --seed 1
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_20 --seed 2
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_20 --seed 3
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_20 --seed 4
---
+++ _train_configs/scripts/pheno_window_5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J pheno_param_mtl_window_5
+#SBATCH -o output/pheno_param_mtl_window_5.out
+#SBATCH -e output/pheno_param_mtl_window_5.err
+#SBATCH -p eecs,share,gpu
+#SBATCH -t 2-12:00:00
+#SBATCH --gres=gpu:1
+
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_5 --seed 0
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_5 --seed 1
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_5 --seed 2
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_5 --seed 3
+python3 -m trainers.train_model --config dmc_mtl/window/pheno_param_mtl_window_5 --seed 4

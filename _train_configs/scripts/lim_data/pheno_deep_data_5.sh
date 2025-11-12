705c95e6c26c59dc63d07a9887b2802145dc4449
William Solow <soloww@submit-a.ib.coehpc>
2025-11-12 08:55:47 -0800

    deep scripts

---
+++ _train_configs/scripts/lim_data/pheno_deep_data_5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J pheno_deep_data_5
+#SBATCH -o output/pheno_deep_data_5.out
+#SBATCH -e output/pheno_deep_data_5.err
+#SBATCH -p eecs,share,gpu
+#SBATCH -t 2-12:00:00
+#SBATCH --gres=gpu:1
+
+python3 -m trainers.train_model --config dmc_mtl/lim_data/deep/pheno_deep_data_5 --seed 0
+python3 -m trainers.train_model --config dmc_mtl/lim_data/deep/pheno_deep_data_5 --seed 1
+python3 -m trainers.train_model --config dmc_mtl/lim_data/deep/pheno_deep_data_5 --seed 2
+python3 -m trainers.train_model --config dmc_mtl/lim_data/deep/pheno_deep_data_5 --seed 3
+python3 -m trainers.train_model --config dmc_mtl/lim_data/deep/pheno_deep_data_5 --seed 4

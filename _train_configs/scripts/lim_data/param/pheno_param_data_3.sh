c7d00bb1549dab889893f0336dcbf563a48af355
William Solow <soloww@cn-gpu3.hpc.engr.oregonstate.edu>
2025-11-13 08:35:57 -0800

    scripts

---
+++ _train_configs/scripts/lim_data/param/pheno_param_data_3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J pheno_param_data_3
+#SBATCH -o output/pheno_param_data_3.out
+#SBATCH -e output/pheno_param_data_3.err
+#SBATCH -p eecs,share,gpu
+#SBATCH -t 2-12:00:00
+#SBATCH --gres=gpu:1
+
+python3 -m trainers.train_model --config dmc_mtl/lim_data/pheno_param_data_3 --seed 0
+python3 -m trainers.train_model --config dmc_mtl/lim_data/pheno_param_data_3 --seed 1
+python3 -m trainers.train_model --config dmc_mtl/lim_data/pheno_param_data_3 --seed 2
+python3 -m trainers.train_model --config dmc_mtl/lim_data/pheno_param_data_3 --seed 3
+python3 -m trainers.train_model --config dmc_mtl/lim_data/pheno_param_data_3 --seed 4
---
+++ _train_configs/scripts/smoothing/ch_param_smoothing0.2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J ch_smoothing0.2
+#SBATCH -o output/ch_smoothing0.2.out
+#SBATCH -e output/ch_smoothing0.2.err
+#SBATCH -p eecs,share,gpu
+#SBATCH -t 2-12:00:00
+#SBATCH --gres=gpu:1
+
+python3 -m trainers.train_model --config dmc_mtl/smoothing/ch_smoothing0.2 --seed 0
+python3 -m trainers.train_model --config dmc_mtl/smoothing/ch_smoothing0.2 --seed 1
+python3 -m trainers.train_model --config dmc_mtl/smoothing/ch_smoothing0.2 --seed 2
+python3 -m trainers.train_model --config dmc_mtl/smoothing/ch_smoothing0.2 --seed 3
+python3 -m trainers.train_model --config dmc_mtl/smoothing/ch_smoothing0.2 --seed 4

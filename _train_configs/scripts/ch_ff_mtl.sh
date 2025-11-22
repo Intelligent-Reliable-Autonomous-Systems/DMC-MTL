dd4dedae8a19994354917929f9bc208f86d5fba4
William Solow <soloww@submit-a.ib.coehpc>
2025-11-21 17:41:50 -0800

    fixed pheno synth experiments

--- _train_configs/scripts/ch_ff_mtl.sh
+++ _train_configs/scripts/ch_ff_mtl.sh
@@ -6,8 +6,8 @@
 #SBATCH -t 1-12:00:00
 #SBATCH --gres=gpu:1
 
-python3 -m trainers.train_model --config dmc_mtl/coldhardiness/ch_param_mtl_ff --seed 0
-python3 -m trainers.train_model --config dmc_mtl/coldhardiness/ch_param_mtl_ff --seed 1
-python3 -m trainers.train_model --config dmc_mtl/coldhardiness/ch_param_mtl_ff --seed 2
-python3 -m trainers.train_model --config dmc_mtl/coldhardiness/ch_param_mtl_ff --seed 3
-python3 -m trainers.train_model --config dmc_mtl/coldhardiness/ch_param_mtl_ff --seed 4
+python3 -m trainers.train_model --config dmc_mtl/grape_coldhardiness/ch_param_mtl_ff --seed 0
+python3 -m trainers.train_model --config dmc_mtl/grape_coldhardiness/ch_param_mtl_ff --seed 1
+python3 -m trainers.train_model --config dmc_mtl/grape_coldhardiness/ch_param_mtl_ff --seed 2
+python3 -m trainers.train_model --config dmc_mtl/grape_coldhardiness/ch_param_mtl_ff --seed 3
+python3 -m trainers.train_model --config dmc_mtl/grape_coldhardiness/ch_param_mtl_ff --seed 4

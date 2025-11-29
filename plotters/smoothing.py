bc1a0aae5254a8d512b2e6cb5cd20dfedcc1eecb
William Solow <soloww@cn-gpu3.hpc.engr.oregonstate.edu>
2025-11-29 13:13:04 -0800

    compile runs sync

--- plotters/smoothing.py
+++ plotters/smoothing.py
@@ -44,19 +44,21 @@ def main():
     ch_models = load_named_pickles(["ColdHardinessSmoothing"], "results_per_cultivars.pkl")
 
     pheno_sorted_keys = np.argsort(list(pheno_models.keys()))
+    print(list(pheno_models.keys()))
+    print(np.array(list(pheno_models.keys()))[pheno_sorted_keys])
     pheno_array = np.array(list(pheno_models.values()))[pheno_sorted_keys]
-
     ch_sorted_keys = np.argsort(list(ch_models.keys()))
     ch_array = np.array(list(ch_models.values()))[ch_sorted_keys]
 
-    # pheno_mean = np.nanmean(pheno_array,axis=(-3, -1))[:,-1]
-    # pheno_std = np.nanstd(pheno_array,axis=(-3, -1))[:,-1]
+    pheno_array[pheno_array == 0] = np.nan
+    pheno_mean = np.nanmean(pheno_array, axis=(1, 2, 3, 4, -1))[:, -1]
+    pheno_std = np.nanstd(pheno_array, axis=(1, 2, 3, 4, -1))[:, -1]
     ch_array[ch_array == 0] = np.nan
     ch_mean = np.nanmean(ch_array, axis=(1, 2, 3, 4, -1))[:, -1]
     ch_std = np.nanstd(ch_array, axis=(1, 2, 3, 4, -1))[:, -1]
     fig, ax = plt.subplots(figsize=(5, 3))
     ax1_color = "tab:blue"
-    # ax.plot(np.arange(6), pheno_mean, color=ax1_color, label="Phenology")
+    ax.plot(np.arange(4), pheno_mean, color=ax1_color, label="Phenology")
     ax.set_xlabel("Smoothing")
     ax.set_xticks(np.arange(4), labels=[0.001, 0.01, 0.1, 0.2])
     ax.set_ylabel("RMSE in Days (Phenology)", color=ax1_color)
@@ -64,7 +66,7 @@ def main():
 
     ax2_color = "tab:red"
     ax2 = ax.twinx()
-    ax2.plot(np.arange(2), ch_mean, color=ax2_color, label="Cold-Hardiness")
+    ax2.plot(np.arange(4), ch_mean, color=ax2_color, label="Cold-Hardiness")
     ax2.set_ylabel(r"RMSE in $^\circ$C (Cold-Hardiness)", color=ax2_color)
     ax2.tick_params(axis="y", labelcolor=ax2_color)
     ax.set_title("Parameter Smoothing Effect on Error")
